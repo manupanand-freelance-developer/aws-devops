450cda67a43a6c7ecddb07f85f0c9c7c499d6bf8
manupanand <manupanandindianinvestor@gmail.com>
2025-03-26 00:33:46 +0530

    fix bug underscore and hiphen

--- vpc-kube-infra/modules/vpc/ngw.tf
+++ vpc-kube-infra/modules/vpc/ngw.tf
@@ -6,8 +6,8 @@
 
 # nat gate way
 resource "aws_nat_gateway" "public_subnet_ntgw" {
-  allocation_id     =  aws_eip.private-vpc-nat-ip.id 
-  subnet_id         =  aws_subnet.public-subnets.id 
+  allocation_id     =  aws_eip.private_vpc_nat_ip.id 
+  subnet_id         =  aws_subnet.public_subnets.id 
 
   tags={
     Name="${var.env}-public-subnet-ntgw"
