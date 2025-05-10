d20e5ba5d9528d4a7aab60cda15d05abe0e064b6
manupanand <manupanandindianinvestor@gmail.com>
2025-05-10 22:18:18 +0530

    hot fix test

--- sonarqube/modules/sonarqube/script.sh
+++ sonarqube/modules/sonarqube/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-touch /tmp/newfile.sh
+
 # Define log file
 LOG_FILE="/var/log/startup_script.log"
 sudo touch $LOG_FILE
@@ -9,9 +9,7 @@ export AWS_USER=${AWS_USER}
 export AWS_PASSWORD=${AWS_PASSWORD}
 export role_name=${role_name}
 export ANSIBLE_HOST_KEY_CHECKING=False
-echo $AWS_USER | tee -a $LOG_FILE
-echo $AWS_PASSWORD | tee -a $LOG_FILE
-echo $role_name | tee -a $LOG_FILE
+
 
 # Redirect stdout and stderr to log file
 
