a60cc1b615338763c2d12c392b252cffbf73ff28
manupanand <96671675+manupanand@users.noreply.github.com>
2025-05-21 01:41:06 +0530

    Delete k8s-infra-selfmanaged directory

--- k8s-infra-selfmanaged/env-dev/state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "devops-state-manupanand-aps2"
-key    = "k8s/dev/terraform.tfstate"
-region = "ap-south-2"
--- k8s-infra-selfmanaged/modules/control-plane/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_control_plane_private_ip" {
-  value = aws_instance.instance_control_plane.private_ip
-}
